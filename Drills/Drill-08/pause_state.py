e3928041a0e992cf428dc3419e88345f63950d14
CHOEHYEONUK <mrbear6500@gmail.com>
2019-10-18 15:18:06 +0900

    drill-08-1 완성
    https://youtu.be/ugkI0_Sidog

---
+++ Drills/Drill-08/pause_state.py
@@ -0,0 +1,41 @@
+import game_framework
+from pico2d import *
+import main_state
+
+name = "PauseState"
+image = None
+
+
+def enter():
+    global image
+    image = load_image('pause.png')
+
+
+def exit():
+    global image
+    del image
+
+
+def update():
+    pass
+
+
+def draw():
+    global image
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if (event.type, event.key) == (SDL_KEYDOWN, SDLK_q):
+            game_framework.pop_state()
+    pass
+
+
+def pause(): pass
+
+
+def resume(): pass
