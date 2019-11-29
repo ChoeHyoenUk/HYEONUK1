2f73617b33a45fe91f3f13c3c95627e07efc72f3
CHOEHYEONUK <2019cgproject@gmail.com>
2019-11-29 15:14:17 +0900

    drill-13 완성
    https://youtu.be/64yLOCri7x4
    2015182042 최현욱

--- Drills/Drill - 13/boy.py
+++ Drills/Drill - 13/boy.py
@@ -159,7 +159,7 @@ class Boy:
 
     def draw(self):
         self.cur_state.draw(self)
-        self.font.draw(self.x - 60, self.y + 50, '(Time: %3.2f)' % (get_time() - self.start_time), (0, 0, 0))
+        self.font.draw(self.x - 60, self.y + 50, '(Time: %3.2f)' % (get_time() - self.start_time), (0,0,0))
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
